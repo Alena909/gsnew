0607c273e6e1900abd2b22ad699c3c10c09463d4
Yelena Guthrie <yelena.guthrie@gmail.com>
2022-12-10 22:16:08 -0600

    replace homepage lottie with custom design, delete extra lottie files

--- src/routes/home/home.component.js
+++ src/routes/home/home.component.js
@@ -1,29 +1,29 @@
-import { HomeContainer } from "./home.styles";
-import Animation from "../../components/animation/animation.components";
-import floralLottie from "../../assets/lotties/57197-floral-border.json";
+import "./home.styles.css";
+import branch from "../../assets/images/flowers/branch_w.png";
+import yellowFlower from "../../assets/images/flowers/yellowFlower_w.png";
+import darkFlower from "../../assets/images/flowers/darkFlower_w.png";
+import lightFlower from "../../assets/images/flowers/lightFlower_w.png";
 
 const Home = () => {
-  const viewport = window.visualViewport;
-  const viewDementions =
-    viewport.width < 450 ? 250 : viewport.width < 760 ? 400 : 500;
-
   return (
-    <HomeContainer>
-      <Animation
-        animationLottie={floralLottie}
-        width={viewDementions}
-        height={viewDementions}
-        style={{ position: "realtive", scale: "1.3", zindex: 5 }}
-      />
-      <h1
-        style={{
-          zIndex: 0,
-          position: "absolute",
-        }}
-      >
-        Welcome
-      </h1>
-    </HomeContainer>
+    <div className="home-container">
+      <div className="home-canvas">
+        <h1>Welcome</h1>
+        <img className="yellowBL" src={yellowFlower} alt="yellow flower" />
+        <img className="yellowTR" src={yellowFlower} alt="yellow flower" />
+        <img className="yellowBR1" src={yellowFlower} alt="yellow flower" />
+        <img className="yellowBR2" src={yellowFlower} alt="yellow flower" />
+        <img className="yellowTL" src={yellowFlower} alt="yellow flower" />
+        <img className="darkTL" src={darkFlower} alt="dark flower" />
+        <img className="darkBR" src={darkFlower} alt="dark flower" />
+        <img className="darkTR" src={darkFlower} alt="dark flower" />
+        <img className="lightTL" src={lightFlower} alt="light flower" />
+        <img className="lightBL" src={lightFlower} alt="light flower" />
+        <img className="branchTL" src={branch} alt="branch" />
+        <img className="branchTR" src={branch} alt="branch" />
+        <img className="branchBL" src={branch} alt="branch" />
+      </div>
+    </div>
   );
 };
 
